ce61aec300f3247654cf3207d302dec22a057b06
Paulo Wender <paulowender2011@gmail.com>
2023-05-23 17:17:15 -0400

    Change project title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
       theme: AppThemes.defaultTheme,
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.HOME,
