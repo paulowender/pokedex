811a3630395a99596a23bc88561d3f2595dd42f2
Paulo Wender <paulowender2011@gmail.com>
2023-06-10 01:31:15 -0400

    Imp test sorting pokemons

--- test/app/controllers/main_controller_test.dart
+++ test/app/controllers/main_controller_test.dart
@@ -7,17 +7,13 @@ void main() {
   test('MainController', () {
     expect(controller, isInstanceOf<MainController>());
   });
-  // setUp(controller.getNextRange);
-  test('Get pokemons', () async {
+
+  setUp(() async {
     await controller.getPokemons();
-    expect(controller.pokemons, hasLength(AppConsts.defaultPageSize));
   });
 
-  test('Get next range', () async {
-    final currentLength = controller.pokemons.length;
-    final nextLength = currentLength + AppConsts.defaultPageSize;
-    await controller.getNextRange();
-    expect(controller.pokemons, hasLength(nextLength));
+  test('Get pokemons', () async {
+    expect(controller.pokemons, hasLength(AppConsts.defaultPageSize));
   });
 
   test('Sort by name', () {
@@ -25,9 +21,30 @@ void main() {
     expect(controller.sortingByName, isTrue);
   });
 
+  test('First Pokemon by name', () async {
+    // await controller.getPokemons();
+    controller.sortByName();
+    expect(controller.pokemons, isNotEmpty);
+    expect(controller.pokemons.first.name, 'Beedrill');
+  });
+
   test('Sort by number', () {
     controller.sortByNumber();
     expect(controller.sortingByName, isFalse);
+  });
+
+  test('First Pokemon by number', () async {
+    // await controller.getPokemons();
+    controller.sortByNumber();
+    expect(controller.pokemons, isNotEmpty);
+    expect(controller.pokemons.first.id, '001');
+  });
+
+  test('Get next range', () async {
+    final currentLength = controller.pokemons.length;
+    final nextLength = currentLength + AppConsts.defaultPageSize;
+    await controller.getNextRange();
+    expect(controller.pokemons, hasLength(nextLength));
   });
 
   // Clean up
